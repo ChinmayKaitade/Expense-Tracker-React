c9d6448d3f0c684d750c5d1f843cf443617e37d0
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-10-31 08:37:03 +0530

    Income and Expense Container code added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/Details/Details";
+
+import useStyles from "./styles";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justifyContent="center"
+        style={{ height: "100vh" }}
+      >
+        {/* Income Container */}
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+
+        {/* Main Container */}
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+
+        {/* Expense Container */}
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
